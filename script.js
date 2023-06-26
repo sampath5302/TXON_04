df62c29f1a906159423b84ce6c3f720fc45a1ab1
sampath5302 <135458047+sampath5302@users.noreply.github.com>
2023-06-26 23:40:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const links = document.querySelectorAll(".page-header ul a");
+for (const link of links) {
+    link.addEventListener("click", clickHandler);
+}
+function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
+    scroll({
+        top: offsetTop,
+        behavior: "smooth"
+    });
+}
